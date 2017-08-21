1cefbf6083981e8a5d8f1b0610a9ca75b7e18bd7
yanqinghao <187280967@qq.com>
2017-08-21 10:25:52 +0800

    111

---
+++ Exercise/ex24.py
@@ -0,0 +1,37 @@
+print "Let's practice everything."
+print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
+
+poem = """
+5 \tThe lovely world
+6 with logic so firmly planted
+7 cannot discern \n the needs of love
+8 nor comprehend passion from intuition
+9 and requires an explanation
+10 \n\t\twhere there is none.
+11 """
+
+print "- - - - - - - - - - - - - - "
+print poem
+print "- - - - - - - - - - - - - - "
+
+
+five = 10 - 2 + 3 - 6 +1
+print "This should be five: %s" % five
+
+def secret_formula(started):
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    crates = jars / 100
+    return jelly_beans, jars, crates
+
+
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
+
+print "With a starting point of: %d" % start_point
+print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
+
+start_point = start_point / 10
+
+print "We can also do that this way:"
+print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
