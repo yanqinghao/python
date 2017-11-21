895b7e2162389480e293e5c133330947b1be4052
yanqinghao <187280967@qq.com>
2017-11-21 17:58:31 +0800

    ml

--- mlpy/sklearn_perceptron.py
+++ mlpy/sklearn_perceptron.py
@@ -7,12 +7,13 @@ Created on Mon Oct 16 17:20:44 2017
 
 from sklearn import datasets
 import numpy as np
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score
 from plot_decision_regions import plot_decision_regions
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
 
 iris = datasets.load_iris()
 X = iris.data[:, [2, 3]]
@@ -25,7 +26,7 @@ sc.fit(X_train)
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
 
-ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
+ppn = Perceptron(max_iter=40, eta0=0.1, random_state=0)
 ppn.fit(X_train_std, y_train)
 y_pred = ppn.predict(X_test_std)
 print('Misclassified samples: %d' % (y_test != y_pred).sum())
@@ -34,6 +35,14 @@ print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
 X_combined_std = np.vstack((X_train_std, X_test_std))
 y_combined = np.hstack((y_train, y_test))
 plot_decision_regions(X=X_combined_std,y=y_combined,classifier=ppn,test_idx=range(105,150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+
+lr = LogisticRegression(C=1000.0, random_state=0)
+lr.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std,y_combined, classifier=lr,test_idx=range(105,150))
 plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
